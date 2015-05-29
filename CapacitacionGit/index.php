30946e6b35ddfb1a4db21c2438a85db07f0c9948
jbriones <jbriones@ubiobio.cl>
2015-05-29 16:46:40 -0300

    Commit Primera Rama, primer cambio

--- CapacitacionGit/index.php
+++ CapacitacionGit/index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo "hola mundo Git";
+        echo "hola mundo Git, primera rama clonada";
         ?>
     </body>
 </html>
